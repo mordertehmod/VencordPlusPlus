e28ec74a54f64dd6a93cac46770a6310af7279fb
zaddi <zaddy@zaddi.dev>
2025-12-02 21:44:29 -0600

    Update themes tab

--- src/components/settings/tabs/themes/ThemeCard.tsx
+++ src/components/settings/tabs/themes/ThemeCard.tsx
@@ -8,7 +8,7 @@ import { Flex } from "@components/Flex";
 import { DeleteIcon } from "@components/Icons";
 import { Link } from "@components/Link";
 import { AddonCard } from "@components/settings/AddonCard";
-import { getThemeInfo, UserThemeHeader } from "@main/themes";
+import { UserThemeHeader } from "@main/themes";
 import { openInviteModal } from "@utils/discord";
 import { showToast } from "@webpack/common";
 
@@ -20,14 +20,11 @@ interface ThemeCardProps {
 }
 
 export function ThemeCard({ theme, enabled, onChange, onDelete }: ThemeCardProps) {
-    // @ts-ignore
-    const themeInfo = getThemeInfo(theme.content, theme.fileName);
     return (
-        console.log("Rendering theme:", themeInfo),
         <AddonCard
-            name={themeInfo.name}
-            description={themeInfo.description}
-            author={themeInfo.author}
+            name={theme.name}
+            description={theme.description}
+            author={theme.author}
             enabled={enabled}
             setEnabled={onChange}
             infoButton={
@@ -38,15 +35,15 @@ export function ThemeCard({ theme, enabled, onChange, onDelete }: ThemeCardProps
                 )
             }
             footer={
-                <Flex flexDirection="row" gap="0.2em">
-                    {!!themeInfo.website && <Link href={themeInfo.website}>Website</Link>}
-                    {!!(themeInfo.website && themeInfo.invite) && " • "}
-                    {!!themeInfo.invite && (
+                <Flex flexDirection="row" style={{ gap: "0.2em" }}>
+                    {!!theme.website && <Link href={theme.website}>Website</Link>}
+                    {!!(theme.website && theme.invite) && " • "}
+                    {!!theme.invite && (
                         <Link
-                            href={`https://discord.gg/${themeInfo.invite}`}
+                            href={`https://discord.gg/${theme.invite}`}
                             onClick={async e => {
                                 e.preventDefault();
-                                themeInfo.invite != null && openInviteModal(themeInfo.invite).catch(() => showToast("Invalid or expired invite"));
+                                theme.invite != null && openInviteModal(theme.invite).catch(() => showToast("Invalid or expired invite"));
                             }}
                         >
                             Discord Server
