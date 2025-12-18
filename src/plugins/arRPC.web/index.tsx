1701f39e189fcfeaf3a7f88926e86eee9a888b00
zaddi <bryce@zaddi.dev>
2025-12-18 12:21:26 -0600

    [Update] - anRPC.web - Fix hidden conditional

--- src/plugins/arRPC.web/index.tsx
+++ src/plugins/arRPC.web/index.tsx
@@ -44,7 +44,7 @@ export default definePlugin({
     description: "Client plugin for arRPC to enable RPC on Discord Web (experimental)",
     authors: [Devs.Ducko],
     reporterTestable: ReporterTestable.None,
-    hidden: IS_VESKTOP || "legcord" in window,
+    hidden: !IS_VESKTOP && !("legcord" in window),
 
     settingsAboutComponent: () => (
         <>
