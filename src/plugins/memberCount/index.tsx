df034d65525041aa0f70aea6082fd5230c7474d5
zaddi <bryce@zaddi.dev>
2025-12-18 13:15:03 -0600

    [Patch Fix] - memberCount - Fix Patch

--- src/plugins/memberCount/index.tsx
+++ src/plugins/memberCount/index.tsx
@@ -36,7 +36,6 @@ export const ThreadMemberListStore = findStoreLazy("ThreadMemberListStore") as F
     getMemberListSections(channelId?: string): { [sectionId: string]: { sectionId: string; userIds: string[]; }; };
 };
 
-
 export const settings = definePluginSettings({
     toolTip: {
         type: OptionType.BOOLEAN,
@@ -73,7 +72,7 @@ export default definePlugin({
             replacement: [
                 {
                     match: /(?<=var\{className:(\i),.+?children):\[(\i\.useMemo[^}]+"aria-multiselectable")/,
-                    replace: ":[$1?.startsWith('members')?$self.render():null,$2",
+                    replace: ":[$1?.includes('members')?$self.render():null,$2",
                 },
             ],
             predicate: () => settings.store.memberList
