e1f4c3fb2753fbfdbae799cbbf247709f6203ac6
LSDZaddi <160318353+mordertehmod@users.noreply.github.com>
2026-01-11 10:28:43 -0600

    [Update] - betterSettings - Remove old code

--- src/plugins/betterSettings/index.tsx
+++ src/plugins/betterSettings/index.tsx
@@ -88,7 +88,8 @@ export default definePlugin({
     settings,
 
     start() {
-        if(settings.store.organizeMenu) enableStyle(fullHeightStyle);
+        if (settings.store.organizeMenu)
+            enableStyle(fullHeightStyle);
     },
 
     stop() {
