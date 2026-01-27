4cff0705fc5ec7f13acb6310f40249a4a971ad6b
LSDZaddi <160318353+mordertehmod@users.noreply.github.com>
2026-01-27 06:33:37 -0600

    [Patch Fix] - searchFix - Fixed patch

--- src/plugins/searchFix/index.tsx
+++ src/plugins/searchFix/index.tsx
@@ -43,7 +43,7 @@ export default definePlugin({
     authors: [Devs.LSDZaddi],
     patches: [
         {
-            find: '"SearchQueryStore");',
+            find: '"SearchQueryStore";',
             replacement: {
                 match: /\i\.searchResultsQuery=(\i)/,
                 replace: "$&,$self.main($1)"
